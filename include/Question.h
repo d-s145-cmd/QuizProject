27632872414824c92d9107ff4481ff1d4b8bbd9f
d-s145-cmd <dashasolovei007@gmail.com>
2026-04-28 22:25:06 +0300

    Update Question.h
    Add all changes

--- include/Question.h
+++ include/Question.h
@@ -1,9 +1,15 @@
 #pragma once
 #include <string>
+#include <vector>
 
 struct Question {
     std::string text;
     std::string answer;
     std::string explanation;
-    std::string image;
+    
+    
+   
+    std::string category;                   
+    int difficulty = 0;                      
+    std::vector<std::string> alternatives;    
 };
