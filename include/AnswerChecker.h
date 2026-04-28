8d47efd1c2891f39811a809d0a36ff3f5d2ca2e6
d-s145-cmd <dashasolovei007@gmail.com>
2026-04-28 22:40:21 +0300

    Update AnswerChecker.h
    Add more arguments

--- include/AnswerChecker.h
+++ include/AnswerChecker.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
+#include <vector> // Обязательно добавляем, так как мы используем std::vector
 
-bool checkAnswer(std::string userAnswer, std::string correctAnswer);
+// Теперь сигнатура (список аргументов) полностью совпадает с .cpp файлом
+bool checkAnswer(std::string userAnswer, std::string correctAnswer, const std::vector<std::string>& alternatives);
 std::string toLower(std::string str);
 std::string trim(const std::string& str);
