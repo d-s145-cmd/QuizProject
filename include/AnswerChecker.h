c4dc50892a35a1d2d091312c5ccdb960837e2eac
d-s145-cmd <dashasolovei007@gmail.com>
2026-04-28 22:43:29 +0300

    Update AnswerChecker.h
    no comments

--- include/AnswerChecker.h
+++ include/AnswerChecker.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <string>
-#include <vector> // Обязательно добавляем, так как мы используем std::vector
+#include <vector>
 
 bool checkAnswer(std::string userAnswer, std::string correctAnswer, const std::vector<std::string>& alternatives);
 std::string toLower(std::string str);
