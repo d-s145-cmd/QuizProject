b7b5ba290d1d8e0ab17af4e2092ece12c1671a4f
d-s145-cmd <dashasolovei007@gmail.com>
2026-04-28 22:41:31 +0300

    Update AnswerChecker.h
    no comments

--- include/AnswerChecker.h
+++ include/AnswerChecker.h
@@ -2,7 +2,6 @@
 #include <string>
 #include <vector> // Обязательно добавляем, так как мы используем std::vector
 
-// Теперь сигнатура (список аргументов) полностью совпадает с .cpp файлом
 bool checkAnswer(std::string userAnswer, std::string correctAnswer, const std::vector<std::string>& alternatives);
 std::string toLower(std::string str);
 std::string trim(const std::string& str);
