590ecfd827d28afad9664a9ecd5c6b99b317736d
d-s145-cmd <dashasolovei007@gmail.com>
2026-04-29 19:42:20 +0300

    Update JsonParser.h
    Added several functions

--- include/JsonParser.h
+++ include/JsonParser.h
@@ -9,7 +9,9 @@ public:
 
 private:
     static std::string readFile(const std::string& filename);
+    static std::string utf8ToCp1251(const std::string& utf8);
     static std::string getStringValue(const std::string& json, const std::string& key, size_t startPos);
+    static std::vector<std::string> getStringArray(const std::string& json, const std::string& key, size_t startPos);
     static void skipWhitespace(const std::string& str, size_t& pos);
     static size_t findNextObject(const std::string& json, size_t start);
 };
