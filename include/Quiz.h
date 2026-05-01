d68ac4373e40c7723f73ebd7de3d14d45a331e2a
d-s145-cmd <dashasolovei007@gmail.com>
2026-05-01 15:41:04 +0300

    Update Quiz.h
    Fixed encoding

--- include/Quiz.h
+++ include/Quiz.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <vector>
 #include "Question.h"
-
 std::vector<Question> createQuestions();
 void printHeader(std::string title);
 void printQuestion(Question q, int num, int total);
