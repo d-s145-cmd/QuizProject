8454341aa724a757aef25812e66ca9c62fb4289d
d-s145-cmd <dashasolovei007@gmail.com>
2026-05-01 15:40:24 +0300

    Update Question.h
    Fixed encoding

--- include/Question.h
+++ include/Question.h
@@ -1,15 +1,11 @@
 #pragma once
 #include <string>
 #include <vector>
-
 struct Question {
     std::string text;
     std::string answer;
     std::string explanation;
-    
-    
-   
-    std::string category;                   
-    int difficulty = 0;                      
-    std::vector<std::string> alternatives;    
+    std::string category;
+    int difficulty = 0;
+    std::vector<std::string> alternatives;
 };
