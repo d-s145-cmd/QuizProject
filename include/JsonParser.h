f8ac202156817e8e4a5a3afc696817f222fd9368
d-s145-cmd <dashasolovei007@gmail.com>
2026-05-02 23:00:51 +0300

    Update JsonParser.h
    Fixed encoding

--- include/JsonParser.h
+++ include/JsonParser.h
@@ -2,17 +2,14 @@
 #include <string>
 #include <vector>
 #include "Question.h"
-
 class JsonParser {
 public:
     static std::vector<Question> loadQuestions(const std::string& filename);
-
 private:
     static std::string readFile(const std::string& filename);
-    static std::string utf8ToCp1251(const std::string& utf8);
-    static std::string getStringValue(const std::string& json, const std::string& key, size_t startPos);
-    static std::vector<std::string> getStringArray(const std::string& json, const std::string& key, size_t startPos);
-    static void skipWhitespace(const std::string& str, size_t& pos);
-    static size_t findNextObject(const std::string& json, size_t start);
-    static int getIntValue(const std::string& json, const std::string& key, size_t startPos);
+    static void skipWhitespace(const std::string& str, std::size_t& pos);
+    static std::string getStringValue(const std::string& json, const std::string& key, std::size_t startPos);
+    static int getIntValue(const std::string& json, const std::string& key, std::size_t startPos);
+    static std::vector<std::string> getStringArray(const std::string& json, const std::string& key, std::size_t startPos);
+    static std::size_t findNextObject(const std::string& json, std::size_t start);
 };
