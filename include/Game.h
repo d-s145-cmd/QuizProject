afd45e45f9cf36c6501de4f6a9cfe3c375e8aade
d-s145-cmd <dashasolovei007@gmail.com>
2026-05-15 20:45:30 +0300

    Add files via upload
    Added structure file

---
+++ include/Game.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include "Question.h"
+#include "ConsoleHelper.h"
+class Game {
+public:
+    
+    Game(const std::vector<Question>& questions);
+
+   
+    void start();
+
+private:
+    std::vector<Question> questions;
+
+    int getPointsByDifficulty(int difficulty) const;
+    std::string getDifficultyLabel(int difficulty) const;
+};
