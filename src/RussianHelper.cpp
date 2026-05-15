dd61cc897667607262b68a5cd58cc870d11c5291
d-s145-cmd <dashasolovei007@gmail.com>
2026-05-15 20:28:29 +0300

    Update RussianHelper.cpp
    Fixed encoding

--- src/RussianHelper.cpp
+++ src/RussianHelper.cpp
@@ -1,6 +1,13 @@
 #include "RussianHelper.h"
 #include <windows.h>
+#ifndef CP_UTF8
+#define CP_UTF8 65001
+#endif
 void setupRussianLocale() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD mode = 0;
+    GetConsoleMode(hConsole, &mode);
+    SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
 }
